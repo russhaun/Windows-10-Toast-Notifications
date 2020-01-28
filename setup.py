5b18d04e101bead209c3c9e2c6f23e72c1382632
Nuno André <mail@nunoand.re>
2020-01-28 14:18:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-
-from operator import attrgetter
-from os import path
+from pathlib import Path
 from setuptools import setup
 
 try:
@@ -10,46 +8,41 @@ except ImportError:
     # pip <= 9.0.3
     from pip.req import parse_requirements
 
-
-def read(fname):
-    return open(path.join(path.dirname(__file__), fname)).read()
-
-
 def from_here(relative_path):
-    return path.join(path.dirname(__file__), relative_path)
+    return Path(__file__).resolve().parent.joinpath(relative_path)
 
-
-requirements_txt = list(map(str, map(
-    attrgetter("req"),
-    parse_requirements(from_here("requirements.txt"), session="")
-)))
+install_requires = [
+    'pywin32',
+    'setuptools',
+    'Pillow',
+]
 
 setup(
-    name="win10toast",
-    version="0.9",
-    install_requires=requirements_txt,
-    packages=["win10toast"],
-    license="BSD",
-    url="https://github.com/jithurjacob/Windows-10-Toast-Notifications",
-    download_url = 'https://github.com/jithurjacob/Windows-10-Toast-Notifications/tarball/0.9',
+    name='win10toast',
+    version='0.9',
+    install_requires=install_requires,
+    packages=['win10toast'],
+    license='MIT',
+    url='https://github.com/nuno-andre/Windows-10-Toast-Notifications',
+    download_url='https://github.com/nuno-andre/Windows-10-Toast-Notifications/archive/master.zip',
     description=(
-        "An easy-to-use Python library for displaying "
-        "Windows 10 Toast Notifications"
+        'An easy-to-use Python library for displaying '
+        'Windows 10 Toast Notifications'
     ),
     include_package_data=True,
     package_data={
         '': ['*.txt'],
         'win10toast': ['data/*.ico'],
     },
-    long_description=read('README.md'),
+    long_description=from_here('README.md').read_text(),
     long_description_content_type='text/markdown',
-    author="Jithu R Jacob",
-    author_email="jithurjacob@gmail.com",
+    author='Jithu R Jacob',
+    author_email='jithurjacob@gmail.com',
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
         'Operating System :: Microsoft',
         'Environment :: Win32 (MS Windows)',
-        "License :: OSI Approved :: MIT License",
+        'License :: OSI Approved :: MIT License',
     ],
 )
